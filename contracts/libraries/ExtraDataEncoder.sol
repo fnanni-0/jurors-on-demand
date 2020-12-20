ebe4693563100a8e4615d105762c709940372fcb
fnanni-0 <fnanni@itba.edu.ar>
2020-12-20 19:59:56 -0300

    feat: add extraData encoder library

---
+++ contracts/libraries/ExtraDataEncoder.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.7.0;
+
+library extraDataEncoder {
+    function encode(
+        uint256 _deadline, 
+        uint256 _minPrice, 
+        uint256 _rulingTimeout, 
+        uint256 _appealTimeout, 
+        address _backupArbitrator, 
+        address[] memory _whiteList,
+        bytes memory _backupArbitratorExtraData
+    ) internal returns(bytes memory extraData) {
+        uint256 whiteListLength = _backupArbitrator.length;
+        extraData = abi.encodePacked(
+            _deadline,
+            _minPrice,
+            _rulingTimeout,
+            _appealTimeout,
+            _backupArbitrator,
+            whiteListLength,
+            _whiteList,
+            backupArbitratorExtraData
+        );
+    }
+}
